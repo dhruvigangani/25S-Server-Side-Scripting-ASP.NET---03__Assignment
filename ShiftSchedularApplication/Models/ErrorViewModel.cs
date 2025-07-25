41802eb7a7f6f800d00446816917b183a7ea91c9
dhruvigangani <dhruvig2020@gmail.com>
2025-07-25 01:05:37 -0400

    Intial Commit

---
+++ ShiftSchedularApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShiftSchedularApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ShiftSchedularApplication/Models/Shift.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Shift
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string EmployeeId { get; set; }  // FK to Identity User
+
+    [Required]
+    public DateTime StartTime { get; set; }
+
+    [Required]
+    public DateTime EndTime { get; set; }
+
+    public bool IsSwapRequested { get; set; }
+    public bool IsGivenAway { get; set; }
+    public bool IsAbsent { get; set; }
+}
